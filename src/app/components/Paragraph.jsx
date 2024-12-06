2eaabf627f528ba6801d893bf9cccf12b0df3d89
Buddhika Madusanaka <119998659+buddhika-madusanka-development@users.noreply.github.com>
2024-12-07 01:12:30 +0530

    setup basic color pallete

--- src/app/components/Paragraph.jsx
+++ src/app/components/Paragraph.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Paragraph = ({paragraphText}) => {
   return (
     <div>
-        <p className='text-muted-paragraph mt-3 leading-6' >{paragraphText}</p>
+        <p className='text-muted-paragraph mt-3 leading-6 text-base-paragraph' >{paragraphText}</p>
     </div>
   )
 }
